9bce59d2b6d323fa73feb2bdbcf7bc9fc776e575
Pedro Pessoa <prp@pobox.com>
2019-02-22 13:49:52 +0000

    add ssh

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,5 @@
 
 envtpl /etc/webdis.donn.json.tpl
 
-/etc/init.d/redis-server start && /usr/local/bin/webdis /etc/webdis.donn.json && bash
+/usr/sbin/sshd && /etc/init.d/redis-server start && /usr/local/bin/webdis /etc/webdis.donn.json && bash
 
